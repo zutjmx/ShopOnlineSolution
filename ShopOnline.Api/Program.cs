42b5a2795ba8153599091dfde4317ea7eadd4be5
Jesus Zuñiga <jesuszt@hotmail.com>
2022-12-29 12:18:04 -0600

    Se agrega proyecto Api y se crea primera migración

---
+++ ShopOnline.Api/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//***IMPORTANT INSTRUCTION HERE - MUST CONFIGURE CONNECTION BEFORE RUNNING MIGRATIONS
+builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnlineConnection"))
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
