c3113b0dfec7d61520231edf3ccda7302e82ac8d
Jesus Zuñiga <jesuszt@hotmail.com>
2022-12-29 17:27:28 -0600

    Modificaciones 29/12/2022 17:27

--- ShopOnline.Api/Program.cs
+++ ShopOnline.Api/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ShopOnline.Api.Data;
+using ShopOnline.Api.Repositories.Contracts;
+using ShopOnline.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnlineConnection"))
 );
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
